4f8580150324c78a5cf0ba491569dedff96e6f6d
Gary <gary909@gmail.com>
2019-05-09 17:22:11 +0200

    cosmic interpolation

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,8 +7,9 @@ import Secret from './pages/Secret';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import api from '../api';
-import logo from '../logo.svg';
+import logo from '../logo.png';
 import Test from './pages/Test';
+import Test2 from './pages/Test2';
 
 export default class App extends Component {
   constructor(props) {
@@ -24,30 +25,44 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">PROJECT Anker</h1>
-          <NavLink to="/" exact>Home</NavLink>
-          <NavLink to="/countries">Countries</NavLink>
-          <NavLink to="/add-country">Add country</NavLink>
-          <NavLink to="/test">Test Here Yeah</NavLink>
-          {!api.isLoggedIn() && <NavLink to="/signup">Signup</NavLink>}
-          {!api.isLoggedIn() && <NavLink to="/login">Login</NavLink>}
-          {api.isLoggedIn() && <Link to="/" onClick={(e) => this.handleLogoutClick(e)}>Logout</Link>}
-          <NavLink to="/secret">Secret</NavLink>
-        </header>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/countries" component={Countries} />
-          <Route path="/add-country" component={AddCountry} />
-          <Route path="/test" component={Test} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Route path="/secret" component={Secret} />
-          <Route render={() => <h2>404</h2>} />
-        </Switch>
-      </div>
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <div className="Title-bar">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">PROJECT Anker</h1>
+            </div>
+
+
+          </header>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/countries" component={Countries} />
+            <Route path="/add-country" component={AddCountry} />
+            <Route path="/test" component={Test} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/test2" component={Test2} />
+            <Route path="/secret" component={Secret} />
+            <Route render={() => <h2>404</h2>} />
+          </Switch>
+        </div>
+
+        <footer className="nav">
+          <div className="lowNav">
+            <div class="App-Header"></div>
+            <NavLink to="/" exact>Home</NavLink>
+            <NavLink to="/countries">Countries</NavLink>
+            <NavLink to="/add-country">Add country</NavLink>
+            <NavLink to="/test">Test</NavLink>
+            {!api.isLoggedIn() && <NavLink to="/signup">Signup</NavLink>}
+            {!api.isLoggedIn() && <NavLink to="/login">Login</NavLink>}
+            {api.isLoggedIn() && <Link to="/" onClick={(e) => this.handleLogoutClick(e)}>Logout</Link>}
+            <NavLink to="/test2">Test2</NavLink>
+            <NavLink to="/secret">Secret</NavLink>
+          </div>
+        </footer>
+      </div >
     );
   }
 }
