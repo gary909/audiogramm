a71f4cc5ce906a020516a04b9be6948d7613c3cb
John Doe <sylvieberger@web.de>
2019-05-13 15:09:52 +0200

    added frontend

---
+++ server/public/precache-manifest.d4764b500f1b98cb4e668d68d891e59a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4241bbfce7f17961e6e378975857fc2e",
+    "url": "/static/media/divver.4241bbfc.mp3"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7c66bc5fe88094e42a91",
+    "url": "/static/js/main.a4f7dbdd.chunk.js"
+  },
+  {
+    "revision": "d0c14dddf7fa5feb1f26",
+    "url": "/static/js/2.0c234887.chunk.js"
+  },
+  {
+    "revision": "7c66bc5fe88094e42a91",
+    "url": "/static/css/main.8effce00.chunk.css"
+  },
+  {
+    "revision": "59659c87355394ee9127360899ab23c8",
+    "url": "/index.html"
+  }
+];
