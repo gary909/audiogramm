35cdf5faef0b6a4a966b3fd90dc852457c53fbad
John Doe <sylvieberger@web.de>
2019-05-14 15:14:15 +0200

    audiofiles can be played

--- server/routes/file-upload-routes.js
+++ server/routes/file-upload-routes.js
@@ -7,15 +7,20 @@ const uploader = require('../configs/cloudinary-setup');
 
 router.post('/upload', uploader.single('videoURL'), (req, res, next) => {
     Audio.create({
-        videoURL: req.file.url
+        videoURL: req.file.url,
+        userId: req.user._id
     }).then(() => console.log('added'));
-    if (!req.file) {
-        next(new Error('No file uploaded!'));
-        return;
-    }
     // get secure_url from the file object and save it in the
     // variable 'secure_url', but this can be any name, just make sure you remember to use the same in frontend
     res.json({ secure_url: req.file.secure_url });
+});
+
+router.get('/upload', (req, res, next) => {
+    Audio.find({
+        userId: req.user._id
+    }).then(results => {
+        res.json(results);
+    });
 });
 
 // get route to get all audio file and send it as json to the front end
