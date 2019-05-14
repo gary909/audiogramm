f0537df30b7f6a74ee755269e61944245d7bb8ac
Gary <gary909@gmail.com>
2019-05-14 18:14:41 +0200

    The perfection of perfection

--- client/src/api.js
+++ client/src/api.js
@@ -88,14 +88,14 @@ export default {
             .get('/upload')
             .then(res => res.data)
             .catch(errHandler);
+    },
+
+
+
+    addcomment(data) {
+        return service
+            .post('/comments', data)
+            .then(res => res.data)
+            .catch(errHandler);
     }
-
-
-
-addcomment(data) {
-  return service
-      .post('/comments', data)
-      .then(res => res.data)
-      .catch(errHandler);
-}
 };
