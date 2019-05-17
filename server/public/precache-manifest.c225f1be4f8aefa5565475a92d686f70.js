ebd934bf42eade8e8f6ca03a919df847c2e886c5
John Doe <sylvieberger@web.de>
2019-05-17 09:49:27 +0200

    built

---
+++ server/public/precache-manifest.c225f1be4f8aefa5565475a92d686f70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "05fbfe63310646eaf8cb",
+    "url": "/static/js/main.314e29cf.chunk.js"
+  },
+  {
+    "revision": "a3f3de2007615345aae1",
+    "url": "/static/js/2.61e71b82.chunk.js"
+  },
+  {
+    "revision": "05fbfe63310646eaf8cb",
+    "url": "/static/css/main.702fc122.chunk.css"
+  },
+  {
+    "revision": "aeec78e60df9fdd88281b717529f8a33",
+    "url": "/index.html"
+  }
+];
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a9ad7730f88fa67fa35cc2a020455b95.js"
+  "/precache-manifest.c225f1be4f8aefa5565475a92d686f70.js"
 );
 
 workbox.clientsClaim();
